99630511fb4366b183deaf0ffd07a610b41a50f6
YannicAd69 <krannic1@web.de>
2022-02-09 09:07:18 +0100

    Ticket

--- Ticketsystem/src/de/bkwest/ticketsystem/model/Ticket.java
+++ Ticketsystem/src/de/bkwest/ticketsystem/model/Ticket.java
@@ -1,5 +1,68 @@
 package de.bkwest.ticketsystem.model;
 
-public class Ticket {
+import java.sql.Date;
 
+public class Ticket {
+	private int id;
+	private String status;
+	private String title;
+	private String discrition;
+	private Date start;
+	private Date end;
+	
+	public Ticket(int id, String status, String title, String discrition, Date start, Date end) {
+		super();
+		this.id = id;
+		this.status = status;
+		this.title = title;
+		this.discrition = discrition;
+		this.start = start;
+		this.end = end;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDiscrition() {
+		return discrition;
+	}
+	public void setDiscrition(String discrition) {
+		this.discrition = discrition;
+	}
+	public Date getStart() {
+		return start;
+	}
+	public void setStart(Date start) {
+		this.start = start;
+	}
+	public Date getEnd() {
+		return end;
+	}
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+	
+	public void notification() {
+		
+	}
+	
+	public void Zuweisung() {
+		
+	}
 }
