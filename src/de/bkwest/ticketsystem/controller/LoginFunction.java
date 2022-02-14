bb96ca814e0731b2c338ed8da62873fddaf70377
YannicAd69 <krannic1@web.de>
2022-02-14 10:35:40 +0100

    User ArrayList in LoginFunction

--- src/de/bkwest/ticketsystem/controller/LoginFunction.java
+++ src/de/bkwest/ticketsystem/controller/LoginFunction.java
@@ -1,5 +1,10 @@
 package de.bkwest.ticketsystem.controller;
 
+import java.util.ArrayList;
+import de.bkwest.ticketsystem.model.User;
+
 public class LoginFunction {
+	
+	ArrayList<User> Users = new ArrayList<User>();
 
 }
