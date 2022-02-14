a231716b3033e94cd1a878a5adcf700e3923c70d
Daniel Hegering <daniel.hegering@bkbocholt-west.eu>
2022-02-14 10:08:31 +0100

    UserInterface -> LoginUserInterface

---
+++ src/de/bkwest/ticketsystem/view/LoginUserInterface.java
@@ -0,0 +1,73 @@
+package de.bkwest.ticketsystem.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+
+public class LoginUserInterface extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtFUsername;
+	private JPasswordField passField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginUserInterface frame = new LoginUserInterface();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginUserInterface() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 345, 280);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblUsername.setBounds(35, 74, 66, 17);
+		contentPane.add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPassword.setBounds(35, 102, 66, 22);
+		contentPane.add(lblPassword);
+		
+		txtFUsername = new JTextField();
+		txtFUsername.setBounds(120, 74, 126, 20);
+		contentPane.add(txtFUsername);
+		txtFUsername.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnLogin.setBounds(112, 149, 89, 23);
+		contentPane.add(btnLogin);
+		
+		passField = new JPasswordField();
+		passField.setBounds(120, 105, 126, 20);
+		contentPane.add(passField);
+	}
+}
+
