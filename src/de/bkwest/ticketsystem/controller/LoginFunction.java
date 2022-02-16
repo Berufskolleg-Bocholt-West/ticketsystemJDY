71f5dda97df37565815f86c0c930afb6b87d7d09
Lukasg09 <goertz-lukas@web.de>
2022-02-16 08:36:49 +0100

    Update LoginFunction.java
    Java.swing
    Window Builder

--- src/de/bkwest/ticketsystem/controller/LoginFunction.java
+++ src/de/bkwest/ticketsystem/controller/LoginFunction.java
@@ -1,6 +1,7 @@
 package de.bkwest.ticketsystem.controller;
 
 import java.util.ArrayList;
+import javax.swing.*; 
 import de.bkwest.ticketsystem.model.User;
 
 public class LoginFunction {
@@ -8,3 +9,15 @@ public class LoginFunction {
 	ArrayList<User> Users = new ArrayList<User>();
 
 }
+{  
+     
+    JButton b1;  
+    JPanel newPanel;  
+    JLabel userLabel, passLabel;  
+    final JTextField  textField1, textField2;  
+      
+}
+    CreateLoginForm()  
+    {     
+
+	}
