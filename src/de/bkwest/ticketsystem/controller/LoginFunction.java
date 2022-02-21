862518a62d746eb8b851e5304c5d6553bf36b637
Daniel Hegering <daniel.hegering@bkbocholt-west.eu>
2022-02-21 14:21:44 +0100

    hihi

--- src/de/bkwest/ticketsystem/controller/LoginFunction.java
+++ src/de/bkwest/ticketsystem/controller/LoginFunction.java
@@ -3,9 +3,9 @@ package de.bkwest.ticketsystem.controller;
 import java.util.ArrayList;
 import javax.swing.*; 
 import de.bkwest.ticketsystem.model.User;
+import de.bkwest.ticketsystem.view.LoginUserInterface;
 
 public class LoginFunction {
-	
 	
 	
 }
