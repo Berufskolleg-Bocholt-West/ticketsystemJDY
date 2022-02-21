70bf9745f943a4a682487a22cdd4b31283f73099
Daniel Hegering <daniel.hegering@bkbocholt-west.eu>
2022-02-21 13:11:53 +0100

    Lukas Änderungen rückgängig gemacht

--- src/de/bkwest/ticketsystem/controller/LoginFunction.java
+++ src/de/bkwest/ticketsystem/controller/LoginFunction.java
@@ -7,17 +7,5 @@ import de.bkwest.ticketsystem.model.User;
 public class LoginFunction {
 	
 	ArrayList<User> Users = new ArrayList<User>();
-
+  
 }
-{  
-     
-    JButton b1;  
-    JPanel newPanel;  
-    JLabel userLabel, passLabel;  
-    final JTextField  textField1, textField2;  
-      
-}
-    CreateLoginForm()  
-    {     
-
-	}
