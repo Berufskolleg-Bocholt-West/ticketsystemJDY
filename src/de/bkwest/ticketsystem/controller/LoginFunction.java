70dd99dec20e9ca62247a29749b29b113f327de5
JasonCo69 <98312024+JasonCo69@users.noreply.github.com>
2022-04-05 15:15:36 +0200

    login funktion  fertig :)

--- src/de/bkwest/ticketsystem/controller/LoginFunction.java
+++ src/de/bkwest/ticketsystem/controller/LoginFunction.java
@@ -6,22 +6,30 @@ import de.bkwest.*;
 import de.bkwest.ticketsystem.model.User;
 
 public class LoginFunction {
-
+	
 	public static boolean compare(String usernameGUI, String passwordGUI) {
+		int i = 0;
 		boolean output;
 		for (User u : DBCon.getAllusers()) {
-			if (u.getUsername().equals(usernameGUI) && u.getPassword().equals(passwordGUI) && u.isBlocked() == false) {
-				System.out.println("User Successfully logged in.");
+			if (u.getUsername().equals(usernameGUI) && u.getPassword().equals(passwordGUI) && !u.isBlocked()) {
+				JOptionPane.showMessageDialog(null, "Login successful");
 				return output = true;
-			} else if (u.getUsername().equals(usernameGUI) && u.isBlocked() == false) {
+			} else if (u.getUsername().equals(usernameGUI) && !u.getPassword().equals(passwordGUI) && u.getAttempts() < 3 && !u.isBlocked()) {
 				u.setAttempts(u.getAttempts() + 1);
+				JOptionPane.showMessageDialog(null, "Login failed");
 				DBCon.updateUserTable(u.getUserID(), u.isBlocked(), u.getAttempts());
-			} else if (u.getAttempts() >= 3 && u.isBlocked() == false) {
+				break;
+			} else if (u.getUsername().equals(usernameGUI) && u.getAttempts() >= 3 ) {
 				u.setBlocked(true);
 				DBCon.updateUserTable(u.getUserID(), u.isBlocked(), u.getAttempts());
-				System.out.println("Login unsuccesful.");
+				JOptionPane.showMessageDialog(null, "User blocked");
 				return output = false;
 			}
+			i++;
+		}
+		if (i ==  DBCon.getAllusers().size()) {
+		JOptionPane.showMessageDialog(null, "Login failed");
+		return output = false;
 		}
 		return output = false;
 	}
